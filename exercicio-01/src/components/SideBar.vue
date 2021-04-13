952dcef3879c2c20a365386734243bdb00411975
Pedro Andrade <pfandrade10@hotmail.com>
2021-04-12 22:42:36 -0300

    criação componentes

---
+++ exercicio-01/src/components/SideBar.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="sidebar">
+        <transition name="slide">
+            <div
+                 class="sidebar-panel">
+                <slot></slot>
+            </div>
+        </transition>
+    </div>
+</template>
+<script>
+    export default {
+        data: () => ({
+            isPanelOpen: true
+        }),
+        methods: {
+            closeSidebarPanel() {
+                this.isPanelOpen = false
+            }
+        }
+    }
+</script>
+<style>
+    .sidebar-panel {
+        overflow-y: auto;
+        background-color: #130f40;
+        position: fixed;
+        left: 0;
+        top: 0;
+        height: 100vh;
+        z-index: 999;
+        padding: 3rem 20px 2rem 20px;
+        width: 250px;
+    }
+</style>
